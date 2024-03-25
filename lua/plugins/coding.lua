371b3c25b7b4b07ba1720c7dd0f9e7c03790918e
Bin Hu <elithrade@gmail.com>
2024-03-25 20:04:55 +0800

    add refactor plugin <leader>r in visual mode

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -5,4 +5,21 @@ return {
     cmd = "IncRename",
     config = true,
   },
+  -- Refactoring tool
+  {
+    "ThePrimeagen/refactoring.nvim",
+    keys = {
+      {
+        "<leader>r",
+        function()
+          require("refactoring").select_refactor()
+        end,
+        mode = "v",
+        noremap = true,
+        silent = true,
+        expr = false,
+      },
+    },
+    opts = {},
+  },
 }
