34a3c12557f3cd397ab67571d217c578af8bbf77
Bin Hu <elithrade@gmail.com>
2024-03-25 20:58:23 +0800

    add surround plugin

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -12,7 +12,9 @@ return {
       {
         "<leader>r",
         function()
-          require("refactoring").select_refactor()
+          require("refactoring").select_refactor({
+            show_success_message = false,
+          })
         end,
         mode = "v",
         noremap = true,
@@ -20,6 +22,22 @@ return {
         expr = false,
       },
     },
+    opts = {},
+  },
+  -- Change surround
+  -- Old text                    Command         New text
+  --------------------------------------------------------------------------------
+  -- surr*ound_words             ysiw)           (surround_words)
+  -- *make strings               ys$"            "make strings"
+  -- [delete ar*ound me!]        ds]             delete around me!
+  -- remove <b>HTML t*ags</b>    dst             remove HTML tags
+  -- 'change quot*es'            cs'"            "change quotes"
+  -- <b>or tag* types</b>        csth1<CR>       <h1>or tag types</h1>
+  -- delete(functi*on calls)     dsf             function calls
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
     opts = {},
   },
 }
