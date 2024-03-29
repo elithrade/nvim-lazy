1260bec6a5a56389804ed260e3957e306f52fbff
Bin Hu <elithrade@gmail.com>
2024-03-29 14:04:21 +0800

    add cmake build step for telescope-fzf-native

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -4,7 +4,7 @@ return {
     dependencies = {
       {
         "nvim-telescope/telescope-fzf-native.nvim",
-        build = "cmake",
+        build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
       },
       "nvim-telescope/telescope-file-browser.nvim",
     },
