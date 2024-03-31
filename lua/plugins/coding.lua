046eb2c9b1e09af9cd577e3ec1cdef2d756bce9a
Bin HU <elithrade@gmail.com>
2024-03-31 21:16:18 +0800

    play around with rustaceanvim configs, capabilities is nil

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -21,4 +21,51 @@ return {
     event = "VeryLazy",
     opts = {},
   },
+  -- TODO copied from https://www.lazyvim.org/extras/lang/rust rustacenvim full spec
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^4", -- Recommended
+    ft = { "rust" },
+    opts = {
+      server = {
+        on_attach = function(_, bufnr)
+          vim.keymap.set("n", "<leader>cR", function()
+            vim.cmd.RustLsp("codeAction")
+          end, { desc = "Code Action", buffer = bufnr })
+          vim.keymap.set("n", "<leader>dr", function()
+            vim.cmd.RustLsp("debuggables")
+          end, { desc = "Rust Debuggables", buffer = bufnr })
+        end,
+        default_settings = {
+          -- rust-analyzer language server configuration
+          ["rust-analyzer"] = {
+            cargo = {
+              allFeatures = true,
+              loadOutDirsFromCheck = true,
+              runBuildScripts = true,
+            },
+            -- Add clippy lints for Rust.
+            checkOnSave = {
+              allFeatures = true,
+              command = "clippy",
+              extraArgs = { "--no-deps" },
+            },
+            procMacro = {
+              enable = true,
+              ignored = {
+                ["async-trait"] = { "async_trait" },
+                ["napi-derive"] = { "napi" },
+                ["async-recursion"] = { "async_recursion" },
+              },
+            },
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      vim.g.rustaceanvim = vim.tbl_deep_extend("keep", vim.g.rustaceanvim or {}, opts or {})
+      -- capabilities is always nil
+      print(vim.g.rustaceanvim.server.capabilities)
+    end,
+  },
 }
