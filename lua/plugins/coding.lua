2fd9cfd438a0c107da3ffc09566b316e0a9d5f94
Bin Hu <elithrade@gmail.com>
2024-06-24 22:02:53 +0800

    update plugin versions; disable rust_analyzer configuration in lspconfig, see https://github.com/mrcjkb/rustaceanvim/discussions/94\#discussioncomment-7813716 for more details

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -24,6 +24,11 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
+      setup = {
+        rust_analyzer = function()
+          return true
+        end,
+      },
       diagnostics = {
         virtual_text = false,
       },
