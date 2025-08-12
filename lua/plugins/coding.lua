d26be62303e0d7c18cc7c805ddefe66c01e66e7e
Bin Hu <elithrade@gmail.com>
2025-08-12 20:21:59 +0800

    replace nvim-surround with mini-surround

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -5,22 +5,6 @@ return {
     cmd = "IncRename",
     config = true,
   },
-  -- Change surround
-  -- Old text                    Command         New text
-  --------------------------------------------------------------------------------
-  -- surr*ound_words             ysiw)           (surround_words)
-  -- *make strings               ys$"            "make strings"
-  -- [delete ar*ound me!]        ds]             delete around me!
-  -- remove <b>HTML t*ags</b>    dst             remove HTML tags
-  -- 'change quot*es'            cs'"            "change quotes"
-  -- <b>or tag* types</b>        csth1<CR>       <h1>or tag types</h1>
-  -- delete(functi*on calls)     dsf             function calls
-  {
-    "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
-    event = "VeryLazy",
-    opts = {},
-  },
   {
     "neovim/nvim-lspconfig",
     opts = {
